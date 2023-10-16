25d0d62a7672e3c25ab853115b97186cfe429d24
Atharv181 <atharvbobade181@gmail.com>
2023-10-16 21:34:37 +0530

    Remove liquidity function added

--- hardhat/contracts/Exchange.sol
+++ hardhat/contracts/Exchange.sol
@@ -41,5 +41,20 @@ contract Exchange is ERC20 {
         return lpTokensToMint;
     }
 
+    function removeLiquidity(uint256 amountOfLPTokens) public returns (uint256, uint256) {
+        require(amountOfLPTokens > 0, "Amount of tokens to remove must be greater than 0");
+
+        uint256 ethReserveBalance = address(this).balance;
+        uint256 lpTokenTotalSupply = totalSupply();
+
+        uint256 ethToReturn = (ethReserveBalance * amountOfLPTokens) / lpTokenTotalSupply;
+        uint256 tokenToReturn = (getReserve() * amountOfLPTokens) / lpTokenTotalSupply;
+
+        _burn(msg.sender, amountOfLPTokens);
+        payable(msg.sender).transfer(ethToReturn);
+        ERC20(tokenAddress).transfer(msg.sender, tokenToReturn);
+
+        return (ethToReturn, tokenToReturn);
+    }
     
 }
